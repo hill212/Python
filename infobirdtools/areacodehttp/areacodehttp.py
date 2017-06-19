09a51a9e41f45f35830683508474ee84041f3608
hill212 <zbs212@163.com>
2017-06-19 17:27:04 +0800

    20170619
    ip,port

--- infobirdtools/areacodehttp/areacodehttp.py
+++ infobirdtools/areacodehttp/areacodehttp.py
@@ -8,11 +8,11 @@
 import urllib3
 
 '''循环按行读取txt文件，文件中按行存储手机号码'''
-for line in open("E:/TestDev/TestCode/test/infobirdtools/areacodehttp/zxno.txt"):
+for line in open("E:/zxno.txt"):
     no = line
     # strip()方法用于移除字符串头尾的指定字符，这里移除换行符，避免输入号码之间有空行
     ygno = no.strip()
-    url = 'http://118.178.123.42:8088/index.html?PhoneNum=' + ygno
+    url = 'http://ip:port/index.html?PhoneNum=' + ygno
     # urllib3 方法发送http get消息，r.data数据 r.head为头信息
     http = urllib3.PoolManager()
     r = http.request('GET', url)
@@ -21,7 +21,7 @@ for line in open("E:/TestDev/TestCode/test/infobirdtools/areacodehttp/zxno.txt")
     s = str(b, encoding="gbk")
     res = ygno + ',' + s
     # print(res)
-    f = open("E:/TestDev/TestCode/test/infobirdtools/areacodehttp/res.txt", "a")
+    f = open("E:/res.txt", "a")
     # 写入文件时每行后面加上换行符
     f.write(res + '\r\n')
     f.close()
